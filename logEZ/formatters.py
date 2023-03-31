8d33d321aa712e9e87714bc46e40766e84f17742
Kunal Gehlot <kunal.g@360hvpl.com>
2023-03-31 08:14:00 +0530

    refactor(formatters): Making the formatters module more Pythonic
    Things considered:
     - Type-hinting
     - Exception Handling
     - Docstrings
     - Linting

--- logEZ/formatters.py
+++ logEZ/formatters.py
@@ -1,6 +1,20 @@
-import logging
+"""Formatters for logEZ: Custom formatters for console and file logging."""
 
-def get_default_formatter():
-    return logging.Formatter(
-        "%(asctime)s - %(name)s : %(levelname)s : %(message)s", "%d-%m-%y %H:%M:%S"
-    )
+import logging
+from logging import Formatter
+
+
+def get_default_formatter() -> Formatter:
+    """Create and return the default formatter for logEZ.
+
+    The default formatter has the following format:
+    '%(asctime)s - %(name)s : %(levelname)s : %(message)s'
+    with a date format of '%d-%m-%y %H:%M:%S'.
+
+    Returns:
+        Formatter: The configured default formatter.
+    """
+    log_format = "%(asctime)s - %(name)s : %(levelname)s : %(message)s"
+    date_format = "%d-%m-%y %H:%M:%S"
+    formatter = logging.Formatter(log_format, date_format)
+    return formatter
