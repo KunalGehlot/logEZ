2737592c0cba5d3c315c56bdf4fc15825184a50b
Kunal Gehlot <kunal.g@360hvpl.com>
2023-03-31 08:15:35 +0530

    refactor(init): Making the init module more Pythonic
    Things considered:
     - Type-hinting
     - Exception Handling
     - Docstrings
     - Linting

--- logEZ/__init__.py
+++ logEZ/__init__.py
@@ -1,5 +1,12 @@
+"""logEZ: A simple, easy-to-use logging library for Python applications."""
+
 from .mylogger import MyLogger
+from .handlers import get_console_handler, get_file_handler
+from .formatters import get_default_formatter
 
-
-def create_logger(*args, **kwargs):
-    return MyLogger(*args, **kwargs)
+__all__ = [
+    "MyLogger",
+    "get_console_handler",
+    "get_file_handler",
+    "get_default_formatter",
+]
