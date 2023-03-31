0fd79072b40581a94d37ea896a323974f380971f
Kunal Gehlot <kunal.g@360hvpl.com>
2023-03-31 08:19:18 +0530

    refactor(setup): Making the setup module more Pythonic
    Things considered:
     - Type-hinting
     - Exception Handling
     - Docstrings
     - Linting
     - Python versions
     - Changelog format

--- setup.py
+++ setup.py
@@ -5,15 +5,24 @@ classifiers = [
     "Intended Audience :: Developers",
     "License :: OSI Approved :: MIT License",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "Operating System :: OS Independent",
 ]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("CHANGELOG.md", "r", encoding="utf-8") as fh:
+    long_description += "\n\n" + fh.read()
 
 setup(
     name="logEZ",
     version="0.0.4",
     description="A simple(r) logger for Python",
-    long_description=open("README.md").read() + "\n\n" + open("CHANGELOG.txt").read(),
-    long_description_content_type='text/markdown',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/KunalGehlot/logEZ",
     author="Zackcodes.ai",
     author_email="gehlotkunal@outlook.com",
@@ -21,5 +30,6 @@ setup(
     classifiers=classifiers,
     keywords="logging",
     packages=find_packages(),
-    install_requires=[""],
+    python_requires=">=3.8",
+    install_requires=[],
 )
