8c1e9c355aaa813c8dc9cf417f8c01762fbfc57c
Kunal Gehlot <kunal.g@360hvpl.com>
2023-03-31 08:12:34 +0530

    refactor(handlers): Making the handlers module more Pythonic
    Things considered:
     - Type-hinting
     - Exception Handling
     - Docstrings
     - Linting

--- logEZ/handlers.py
+++ logEZ/handlers.py
@@ -1,12 +1,33 @@
+"""Handlers for logEZ: Custom handlers for console and file logging."""
+
 import logging
-
-def get_console_handler(formatter):
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(formatter)
-    return consoleHandler
+from logging import FileHandler, StreamHandler, Formatter
 
 
-def get_file_handler(log_file_name, formatter):
-    fileHandler = logging.FileHandler(log_file_name)
-    fileHandler.setFormatter(formatter)
-    return fileHandler
+def get_console_handler(formatter: Formatter) -> StreamHandler:
+    """Create and return a console handler with the specified formatter.
+
+    Args:
+        formatter (Formatter): The formatter to use for the console handler.
+
+    Returns:
+        StreamHandler: The configured console handler.
+    """
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    return console_handler
+
+
+def get_file_handler(log_file_name: str, formatter: Formatter) -> FileHandler:
+    """Create and return a file handler with the specified formatter and log file name.
+
+    Args:
+        log_file_name (str): The name of the log file to write logs to.
+        formatter (Formatter): The formatter to use for the file handler.
+
+    Returns:
+        FileHandler: The configured file handler.
+    """
+    file_handler = logging.FileHandler(log_file_name)
+    file_handler.setFormatter(formatter)
+    return file_handler
